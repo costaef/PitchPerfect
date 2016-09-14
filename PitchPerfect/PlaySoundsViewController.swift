3238a44d9b89dd193f66d51e2e6a13bd6c73f81c
Eduardo Costa <ed_costa@hotmail.com>
2016-09-14 11:43:19 -0300

    Add buttons

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class PlaySoundsViewController: UIViewController {
 
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var darthVaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,4 +40,13 @@ class PlaySoundsViewController: UIViewController {
     }
     */
 
+    // MARK: - Actions
+    
+    @IBAction func playButtonTouch(_ sender: UIButton) {
+        print("Play Button with Tag: \(sender.tag)")
+    }
+    
+    @IBAction func stopButtonTouch(_ sender: UIButton) {
+        print("Stop Button...")
+    }
 }
